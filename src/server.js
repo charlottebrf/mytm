91557d3eb298608292812870565336779759e277
wemmm <jenny.wem@gmail.com>
2017-07-04 12:24:46 +0100

    Configures index.html to show

--- src/server.js
+++ src/server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const namer = require('./namer');
-const app = express()
+const app = express();
+const path = require('path');
 
 const port = 8000;
 
 app.get('/:name', (req, res) => {
   var name = req.params.name;
   res.send(namer.greet(name));
+});
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(port, () => {
--- test/featureTest.spec.js
+++ test/featureTest.spec.js
@@ -1,9 +1,6 @@
-var app = require('../src/server')
 const Browser = require('zombie');
 var http = require('http');
 
-// We're going to make requests to http://example.com/signup
-// Which will be routed to our test server localhost:3000
 describe('User visits app', function() {
 
   before(function() {
@@ -12,12 +9,12 @@ describe('User visits app', function() {
       res.end('okay');
     });
 
-    this.server.listen(8000, '127.0.0.1');
-    this.browser = new Browser({ site: 'http://localhost:8000'});
+    this.server.listen(8080, '127.0.0.1');
+    this.browser = new Browser({ site: 'http://localhost:8080'});
   })
 
 before(function(done) {
-  this.browser.visit('/namer', done);
+  this.browser.visit('/', done);
 });
 
 it('should be successful', function() {
@@ -25,7 +22,7 @@ it('should be successful', function() {
 });
 
 it('should see welcome page', function() {
-  this.browser.assert.text('title', 'Welcome To Mind Mapp');
+  this.browser.assert.text('title', 'Welcome to Mind Mapp');
 });
 
 });
