7ff3a3d6aaa78bfd1558fd023a74a279b6ae9bd2
Michael Jacobson <michael.jacobson89@gmail.com>
2017-07-05 11:05:11 +0100

    • minor refactor - changed paths to directories inside controller • all working now :)

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,19 +1,8 @@
 $(document).ready(function() {
   $('#b').click(function(){
-    var c = document.getElementById("canvas");
-    var ctx = c.getContext("2d");
+    let c = document.getElementById("canvas");
+    let ctx = c.getContext("2d");
     ctx.fillStyle = "cornflowerblue";
     ctx.fillRect(20, 20, 150, 100);
   })
-})
-
-
-// $(mybutton).onClick(function() {
-//   all_nodes << Idea(...);
-// })
-//
-// var all_nodes = [great, other, more thing];
-//
-// all_nodes.forEach(function(element) {
-//   my_super_canvas_drawing_thing(element);
-// })
+});
