0591cdf1247b864a5e604fa2119448fc261b582d
Charlotte Fereday <cbrfereday@gmail.com>
2017-07-05 16:25:31 +0100

    Adds Idea model for an object inside the Canvas

--- src/idea.js
+++ src/idea.js
@@ -1,28 +1,20 @@
-// 'use strict';
+class Idea {
 
-function Idea(text) {
-  return {
-    text: text,
-    location: {x: 20, y: 20},
-    size: { width: 150, height: 100},
-    color: "cornflowerblue"
+  constructor(x, y, w, h, fill) {
+  this.x = x || 10;
+  this.y = y || 10;
+  this.w = w || 1;
+  this.h = h || 1;
+  this.fill = fill || "#76A9DC";
+  }
 
-    move: function(x,y) {
-      this.location.x += x;
-      this.location.y += y;
-    }
+  draw(ctx) {
+    ctx.fillStyle = this.fill;
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+  }
 
-    resize: function(factor) {
-      this.size.width = this.size.width * factor;
-      this.size.height = this.size.height * factor;
-    }
+  contains(mx, my) {
+    return(this.x <= mx) && (this.x + this.w >= mx) && (this.y <= my) && (this.y + this.h >= my);
+  }
 
-    important: function() {
-      this.color = "devil-red";
-    }
 }
-
-var great = Idea("Make America Great Again");
-great.important();
-great.move(50, 10);
-great.resize(3);
