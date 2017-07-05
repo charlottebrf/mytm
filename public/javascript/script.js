797184c29ef7a4ca08ffd79d29db58f1d32702fb
Charlotte Fereday <cbrfereday@gmail.com>
2017-07-05 10:45:09 +0100

    add second button to index view and add click event for it in script.js

--- public/javascript/script.js
+++ public/javascript/script.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
-  $('#b').click(function(){
+  $('#a').click(function(){
     var c = document.getElementById("canvas");
     var ctx = c.getContext("2d");
+    console.log(ctx);
     ctx.fillStyle = "cornflowerblue";
     ctx.fillRect(20, 20, 150, 100);
+  })
+  $('#b').click(function() {
+    alert(ctx);
   })
 })
 
