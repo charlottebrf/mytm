9c374e7fe893f77cd0b5f57beee828f62196adbb
Charlotte Fereday <cbrfereday@gmail.com>
2017-07-06 16:25:22 +0100

    Adds text spike

--- public/javascript/idea.js
+++ public/javascript/idea.js
@@ -1,6 +1,6 @@
 class Idea {
 
-  constructor(x, y, color) {
+  constructor(x, y, color, text) {
   this.x = x || 10;
   this.y = y || 10;
   this.color = color || "#76A9DC";
@@ -12,14 +12,25 @@ class Idea {
   // Cirlce
   this.radius = 10;
   this.startAngle = 0;
-  this.endAngle = 2 * Math.PI
+  this.endAngle = 2 * Math.PI;
+
+  //text box
+  this.text = text;
+  this.font = '12px Arial';
+  this.fontColor = 'black';
+
   }
 }
 
-Idea.drawRectangle = function(context, x, y, color) {
+Idea.drawRectangle = function(context, x, y, color, text) {
   idea = new Idea(x, y, color);
   context.fillStyle = idea.color;
   context.fillRect(idea.x, idea.y, idea.w, idea.h);
+  context.textBaseline = 'middle';
+  context.font = this.font;
+  context.fillStyle = this.fontColor;
+  context.fillText(this.text, x, y);
+
 }
 
 Idea.drawCircle = function(context, x, y, color) {
--- public/javascript/interface.js
+++ public/javascript/interface.js
@@ -5,14 +5,27 @@ document.addEventListener("DOMContentLoaded", function() {
     return colors[Math.floor((Math.random() * colors.length), 0)]
   }
 
+
   let htmlCanvas = document.getElementById("canvas");
   let canvas = new Canvas(htmlCanvas);
   let rectangle = document.getElementById("rectangle");
   let circle = document.getElementById("circle");
   let reset = document.getElementById("reset");
+  // let input = document.createElement('input');
+  // let font = '14px sans-serif'
+  // let hasInput = false
+
+  // canvas.onclick = function(e) {
+  //   if (hasInput) return;
+  //   addInput(e.clientX, e.clientY);
+  // }
+  //
+  // canvas.onclick = function() {
+  //   canvas.addText()
+  // }
 
   rectangle.onclick = function() {
-    canvas.addIdea(Idea.drawRectangle(canvas.context, Math.floor((Math.random() * canvas.width), 1), Math.floor((Math.random() * canvas.height), 1), colorPicker()));
+    canvas.addIdea(Idea.drawRectangle(canvas.context, Math.floor((Math.random() * canvas.width), 1), Math.floor((Math.random() * canvas.height), 1), colorPicker(), "Hello!"));
   };
 
   circle.onclick = function() {
@@ -22,4 +35,6 @@ document.addEventListener("DOMContentLoaded", function() {
   reset.onclick = function() {
     canvas.reset()
   };
+
+
 });
