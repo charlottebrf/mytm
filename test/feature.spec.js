d39d22d6806eb19f70a54d44a11ab1c7c4e5d918
Charlotte Fereday <cbrfereday@gmail.com>
2017-07-07 12:24:53 +0100

    Refators the server to model the unit tests on

--- test/feature.spec.js
+++ test/feature.spec.js
@@ -1,26 +1,26 @@
-const http = require('http');
-process.env.NODE_ENV = 'test';
-const app = require('../src/server');
-const Browser = require('zombie');
-
-describe('User visits app', function() {
-
-  before(function() {
-    this.server = http.createServer(app).listen(8080);
-    this.browser = new Browser({ site: 'http://localhost:8080'});
-  });
-
-  before (function(done) {
-    this.browser.visit('/DrChaz', done);
-  });
-
-  it('should see welcome page', function() {
-    // this.browser.assert.success
-    this.browser.assert.text('title', 'Hi, DrChaz!');
-  });
-
-  after(function(done) {
-    this.server.close(done);
-  });
-
-});
+// const http = require('http');
+// process.env.NODE_ENV = 'test';
+// const app = require('../src/server');
+// const Browser = require('zombie');
+//
+// describe('User visits app', function() {
+//
+//   before(function() {
+//     this.server = http.createServer(app).listen(8080);
+//     this.browser = new Browser({ site: 'http://localhost:8080'});
+//   });
+//
+//   before (function(done) {
+//     this.browser.visit('/DrChaz', done);
+//   });
+//
+//   it('should see welcome page', function() {
+//     // this.browser.assert.success
+//     this.browser.assert.text('title', 'Hi, DrChaz!');
+//   });
+//
+//   after(function(done) {
+//     this.server.close(done);
+//   });
+//
+// });
---
+++ test/idea.spec.js
@@ -0,0 +1,37 @@
+const http = require('http');
+process.env.NODE_ENV = 'test';
+const app = require('../src/server');
+const idea = require('../public/javascript/idea.js');
+const Browser = require('zombie');
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('User sees blank canvas', function() {
+
+  before(function() {
+    this.server = http.createServer(app).listen(8000);
+    this.browser = new Browser({ site: 'http://localhost:8000'});
+  });
+
+  before (function(done) {
+    this.browser.visit('/index', done);
+  });
+
+  
+  //
+  // it('should have a blank canvas', function() {
+  //   this.browser.assert.element('#canvas');
+  // });
+  //
+  // it('should show an addded object', function() {
+  //   canvasObject = new Idea(document.getElementById('canvas').getContext('2d'));
+  //   // canvasObject.context.fillStyle = "cornflowerblue"
+  //   this.browser.assert.element('#canvas1')
+  //   // canvasObject.context.clear();
+  // });
+
+  after(function(done) {
+    this.server.close(done);
+  });
+
+});
