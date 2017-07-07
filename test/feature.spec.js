21dd391cf5d69043c8def56342712910958747ed
Charlotte Fereday <cbrfereday@gmail.com>
2017-07-07 15:44:42 +0100

    Comments out other files

--- test/feature.spec.js
+++ test/feature.spec.js
@@ -1,26 +1,27 @@
-// const http = require('http');
-// process.env.NODE_ENV = 'test';
-// const app = require('../src/server');
-// const Browser = require('zombie');
-//
-// describe('User visits app', function() {
-//
-//   before(function() {
-//     this.server = http.createServer(app).listen(8080);
-//     this.browser = new Browser({ site: 'http://localhost:8080'});
-//   });
-//
-//   before (function(done) {
-//     this.browser.visit('/DrChaz', done);
-//   });
-//
-//   it('should see welcome page', function() {
-//     // this.browser.assert.success
-//     this.browser.assert.text('title', 'Hi, DrChaz!');
-//   });
-//
-//   after(function(done) {
-//     this.server.close(done);
-//   });
-//
-// });
+const app = require('../src/server');
+Browser = require('zombie');
+let assert = require('chai').assert;
+describe("zombie suite", function() {
+
+  before(function() {
+    this.server = app.listen(8000);
+    this.browser = new Browser({ site: 'http://localhost:8000'});
+  });
+
+  before(function(done) {
+    this.browser.visit('/', function() {
+      done();
+    });
+  });
+
+
+  after(function(done) {
+    this.server.close(done);
+  });
+
+  it('finds a canvas on the homepage', function(done) {
+    var browser = this.browser;
+    browser.assert.text('h1', 'mind mapp');
+    done();
+  });
+});
--- test/idea.spec.js
+++ test/idea.spec.js
@@ -1,100 +1,27 @@
-const app = require('../src/server');
-Browser = require('zombie');
-let assert = require('chai').assert;
-describe("zombie suite", function() {
-
-  before(function() {
-    this.server = app.listen(8000);
-    this.browser = new Browser({ site: 'http://localhost:8000'});
-  });
-
-  before(function(done) {
-    this.browser.visit('/', function() {
-      done();
-    });
-  });
-
-
-  after(function(done) {
-    this.server.close(done);
-  });
-
-  it('finds a canvas on the homepage', function(done) {
-    var browser = this.browser;
-    browser.assert.text('h1', 'mind mapp');
-    done();
-  });
-});
-
-
-
+// const app = require('../src/server');
 // Browser = require('zombie');
 // let assert = require('chai').assert;
-// // let request = require('supertest');
+// describe("zombie suite", function() {
 //
-// describe('loading express', function() {
-//   let server;
-//   beforeEach(function() {
-//     delete require.cache[require.resolve('../src/server')];
-//     server = require('../src/server');
+//   before(function() {
+//     this.server = app.listen(8000);
+//     this.browser = new Browser({ site: 'http://localhost:8000'});
 //   });
-//   afterEach(function(done) {
-//     server.close(done);
-//   });
-//
-//   beforeEach(function() {
-//     browser = new Browser();
-//   });
-//
-//   b
-
-  // it('should visit the home page', function(done) {
-  //   browser.visit('/', function() {
-  //     assert(browser.text("#h1") === "mind mapp",
-  //       "heading app must be present");
-  //
-  //       done();
-  //   })
-  // })
-
-// describe('User visits index page', function() {
-//   const browser = new Browser();
 //
 //   before(function(done) {
-//     browser.visit('/', done);
+//     this.browser.visit('/', function() {
+//       done();
+//     });
 //   });
 //
-//   it('sees a canvas when page is loaded', function() {
-//      browser.assert.element('#canvas');
+//
+//   after(function(done) {
+//     this.server.close(done);
 //   });
-//
-//
-// })
+//   //
+//   // it('finds a canvas on the homepage', function(done) {
+//   //   var browser = this.browser;
+//   //   browser.assert.text('h1', 'mind mapp');
+//   //   done();
+//   // });
 // });
-
-
- // const http = require('http');
- //  process.env.NODE_ENV = 'test';
- //  const app = require('../src/server');
- //  const Browser = require('zombie');
- //
- //  describe('User sees blank canvas', function() {
- //
- //    before(function() {
- //      this.server = http.createServer(app).listen(8080);
- //      this.browser = new Browser({ site: 'http://localhost:8080'});
- //    });
- //
- //    before (function(done) {
- //      this.browser.visit('/', done);
- //    });
- //
- //    it('should have a blank canvas', function() {
- //      this.browser.assert.element('#canvas');
- //    });
- //
- //    after(function(done) {
- //      this.server.close(done);
- //    });
- //
- //  });
