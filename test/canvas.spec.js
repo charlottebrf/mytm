4e5113ddc29ef398081ec3521ccc7c4e161dce23
Charlotte Fereday <cbrfereday@gmail.com>
2017-07-08 11:40:56 +0100

    Fixes draw scope errors & adds line spec

--- test/canvas.spec.js
+++ test/canvas.spec.js
@@ -1,35 +1,28 @@
-// const http = require('http');
-// process.env.NODE_ENV = 'test';
-// const app = require('../src/server');
-// const idea = require('../public/javascript/idea.js');
-// const Browser = require('zombie');
-// const chai = require('chai');
-// const expect = chai.expect;
-//
-// describe('User sees blank canvas', function() {
-//
-//   before(function() {
-//     this.server = http.createServer(app).listen(8080);
-//     this.browser = new Browser({ site: 'http://localhost:8080'});
-//   });
-//
-//   before (function(done) {
-//     this.browser.visit('/index', done);
-//   });
-//
-//   it('should have a blank canvas', function() {
-//     this.browser.assert.element('#canvas');
-//   });
-//
-//   it('should show an addded object', function() {
-//     canvasObject = new Idea(document.getElementById('canvas').getContext('2d'));
-//     // canvasObject.context.fillStyle = "cornflowerblue"
-//     this.browser.assert.element('#canvas1')
-//     // canvasObject.context.clear();
-//   });
-//
-//   after(function(done) {
-//     this.server.close(done);
-//   });
-//
-// });
+const app = require('../src/server');
+Browser = require('zombie');
+let assert = require('chai').assert;
+describe("zombie suite", function() {
+
+  before(function() {
+    this.server = app.listen(8000);
+    this.browser = new Browser({ site: 'http://localhost:8000'});
+  });
+
+  before(function(done) {
+    this.browser.visit('/', function() {
+      done();
+    });
+  });
+
+  after(function(done) {
+    this.server.close(done);
+  });
+
+  it('has a mind mapp header', function(done) {
+    var browser = this.browser;
+    browser.assert.text('h1', 'mind mapp');
+    done();
+  });
+
+
+});
